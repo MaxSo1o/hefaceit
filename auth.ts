d217ad40ea440383f06f589992801518253c27bb
Maksim <maxso1o@yndex.ru>
2025-04-14 20:26:42 +0300

    1

--- auth.ts
+++ auth.ts
@@ -8,9 +8,20 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     })],
     secret: process.env.AUTH_SECRET,
     callbacks: {
-        async session({ session, user }) {
-            session.user.id = user.id
+        async jwt({ token, account }) {
+            if (account) {
+                token.accessToken = account.access_token
+            }
+            return token
+        },
+        async session({ session, token }) {
+            session.user.accessToken = token.accessToken
             return session
+        },
+        async redirect({ url, baseUrl }) {
+            if (url.startsWith("/")) return `${baseUrl}${url}`
+            else if (new URL(url).origin === baseUrl) return url
+            return baseUrl
         }
     }
 })
