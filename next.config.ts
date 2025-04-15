043770e6344bd70a90e000034491a3fc8894899a
Maksim <maxso1o@yndex.ru>
2025-04-15 12:54:24 +0300

    1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     distDir: "out",
+    allowedDevOrigins: ['hefaceit.ru', '*.hefaceit.ru'],
 };
 
 export default nextConfig;
