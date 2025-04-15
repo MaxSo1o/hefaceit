0bac22858394bac10b25ec4f24cc3f560634ac13
Maksim <maxso1o@yndex.ru>
2025-04-15 13:13:20 +0300

    1

--- auth.ts
+++ auth.ts
@@ -5,11 +5,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     providers: [FaceIt({
         clientId: process.env.FACEIT_CLIENT_ID,
         clientSecret: process.env.FACEIT_CLIENT_SECRET,
-        issuer: 'https://api.faceit.com/auth',
-        userinfo: 'https://api.faceit.com/auth/v1/resources/userinfo',
-        jwks_endpoint: 'https://api.faceit.com/auth/v1/oauth/certs',
-        token: 'https://api.faceit.com/auth/v1/oauth/token',
-        authorization: 'https://accounts.faceit.com'
     })],
     session: {
         strategy: "jwt",
