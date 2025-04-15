d2b5692bdb72dbc9ec398480fd14d0ac3b945c19
Maksim <maxso1o@yndex.ru>
2025-04-15 13:50:54 +0300

    1

--- auth.ts
+++ auth.ts
@@ -1,32 +1,50 @@
 import NextAuth from "next-auth"
-import FaceIt from "next-auth/providers/faceit"
+import { OAuthConfig } from "next-auth/providers"
+import type { Profile } from "next-auth"
+
+const FaceItProvider = {
+    id: "faceit",
+    name: "FaceIt",
+    type: "oauth",
+    version: "2.0",
+    issuer: "https://api.faceit.com/auth",
+    authorization: "https://accounts.faceit.com",
+    token: "https://api.faceit.com/auth/v1/oauth/token",
+    userinfo: "https://api.faceit.com/auth/v1/resources/userinfo",
+    jwks_endpoint:'https://api.faceit.com/auth/v1/oauth/certs',
+    clientId: process.env.FACEIT_CLIENT_ID,
+    clientSecret: process.env.FACEIT_CLIENT_SECRET,
+    profile(profile) {
+        return {
+            id: profile.player_id,
+            name: profile.nickname,
+            email: profile.email || null,
+            image: profile.avatar || null
+        }
+    }
+} satisfies OAuthConfig<Profile>
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    providers: [FaceIt({
-        clientId: process.env.FACEIT_CLIENT_ID,
-        clientSecret: process.env.FACEIT_CLIENT_SECRET,
-    })],
+    providers: [FaceItProvider],
     session: {
         strategy: "jwt",
     },
     callbacks: {
         async signIn({ account, profile }) {
-            console.log("Sign in callback triggered");
-            return true;
+            console.log("✅ signIn", account, profile)
+            return true
         },
         async jwt({ token, account, profile }) {
             if (account) {
-                token.accessToken = account.access_token;
-                token.refreshToken = account.refresh_token;
-                token.providerAccountId = account.providerAccountId;
+                token.accessToken = account.access_token
+                token.providerAccountId = account.providerAccountId
             }
-            return token;
+            return token
         },
         async session({ session, token }) {
-            session.accessToken = token.accessToken;
-            session.providerAccountId = token.providerAccountId;
-            return session;
-        }
+            session.accessToken = token.accessToken
+            session.providerAccountId = token.providerAccountId
+            return session
+        },
     }
-
 })
