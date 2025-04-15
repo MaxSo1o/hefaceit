ec89731d5cb51b6605fc56a08eee749371e1ed04
Maksim <maxso1o@yndex.ru>
2025-04-15 14:34:27 +0300

    1

--- auth.ts
+++ auth.ts
@@ -1,50 +1,21 @@
 import NextAuth from "next-auth"
-import { OAuthConfig } from "next-auth/providers"
-import type { Profile } from "next-auth"
-
-const FaceItProvider = {
-    id: "faceit",
-    name: "FaceIt",
-    type: "oauth",
-    version: "2.0",
-    issuer: "https://api.faceit.com/auth",
-    authorization: "https://accounts.faceit.com",
-    token: "https://api.faceit.com/auth/v1/oauth/token",
-    userinfo: "https://api.faceit.com/auth/v1/resources/userinfo",
-    jwks_endpoint:'https://api.faceit.com/auth/v1/oauth/certs',
-    clientId: process.env.FACEIT_CLIENT_ID,
-    clientSecret: process.env.FACEIT_CLIENT_SECRET,
-    profile(profile) {
-        return {
-            id: profile.player_id,
-            name: profile.nickname,
-            email: profile.email || null,
-            image: profile.avatar || null
-        }
-    }
-} satisfies OAuthConfig<Profile>
+import FaceIt from "next-auth/providers/faceit"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    providers: [FaceItProvider],
+    providers: [FaceIt({
+        clientId: process.env.FACEIT_CLIENT_ID,
+        clientSecret: process.env.FACEIT_CLIENT_SECRET,
+    })],
     session: {
         strategy: "jwt",
     },
     callbacks: {
-        async signIn({ account, profile }) {
-            console.log("✅ signIn", account, profile)
-            return true
-        },
-        async jwt({ token, account, profile }) {
+        async jwt({ token, account }) {
             if (account) {
+                console.log(account)
                 token.accessToken = account.access_token
-                token.providerAccountId = account.providerAccountId
             }
             return token
         },
-        async session({ session, token }) {
-            session.accessToken = token.accessToken
-            session.providerAccountId = token.providerAccountId
-            return session
-        },
-    }
+    },
 })
