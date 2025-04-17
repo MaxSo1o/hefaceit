b30ebe1ea4eeca231d5e9370a6310bab5654dca5
Maksim <maxso1o@yndex.ru>
2025-04-17 22:22:33 +0300

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,17 +3,10 @@ import {auth, signOut} from "@/auth";
 export default function Dashboard() {
     const session = auth()
 
-    if (!session) {
-        return (
-            <div className="auth-container">
-                Вы авторизованы {session.user?.nickname}
-            </div>
-        )
-    }
-
     return (
         <div className="auth-container">
             <div className="box">
+                <span>Ваша авторизация прошла успешно {session?.user?.name}</span>
                 <button onClick={async () => {
                     "use server"
                     await signOut()
