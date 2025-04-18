aaba2a81b64c9e74c0cd6735d540e103c40e33ac
MaxSo1o <maxso1o@yandex.ru>
2025-04-18 09:06:14 +0300

    update auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,12 @@ import {auth, signOut} from "@/auth";
 
 export default function Dashboard() {
     const session = auth()
+    console.log(session)
 
     return (
         <div className="auth-container">
             <div className="box">
-                <span>Ваша авторизация прошла успешно {session}</span>
+                <span>Ваша авторизация прошла успешно</span>
                 <button onClick={async () => {
                     "use server"
                     await signOut()
